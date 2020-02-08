61f0118fbdd10cf30f6552d54839fa5235b24f0e
DQuinaluisa <dft.quinaluisa@yavirac.edu.ec>
2020-02-07 21:10:50 -0500

    WEB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, BrowserRouter } from 'react-router-dom';
+import Titulo from './components/Titulo';
+import Navigation from './components/Navigation';
+import Informe from './components/Informe';
+import Salas from './components/Salas';
+import './index.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Navigation/>
+        
+
+
+        <Route path="/" exact component={Titulo} />
+        <Route path="/create" component={Salas} />
+        <Route path="/getInfo" component={Informe} />
+      </BrowserRouter>
   );
 }
 
